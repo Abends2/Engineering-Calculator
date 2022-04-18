a0bec85a673514619b7e75834d58c7fa8b28107e
Abends2 <98419421+Abends2@users.noreply.github.com>
2022-04-18 22:14:10 +0300

    rewrote the struct of calculation

--- Calculator.py
+++ Calculator.py
@@ -3,91 +3,110 @@ from tkinter import messagebox
 import tkinter.font as font
 import math
 
-
 # 40C3EC - color for text on Buttons
 # fffff0 - background color
 # 40C3EC - foreground color
 
-window = Tk()
-window.title("Engineering Calculator")
-window.iconbitmap('./assets/icon.ico')
-window.geometry('345x750')
-window.resizable(width=False, height=False)
-window.configure(bg='#fffff0', borderwidth=8, relief='solid')
 
-# set the font
-f1 = font.Font(family='Calibri', size=12, weight='bold')
+def main():
+    # window's settings
+    window = Tk()
+    window.title("Engineering Calculator")
+    window.iconbitmap('./assets/icon.ico')
+    window.geometry('345x750')
+    window.resizable(width=False, height=False)
+    window.configure(bg='#fffff0', borderwidth=8, relief='solid')
 
-# entry unit
-entry = Entry(window, width=52, relief='solid', borderwidth=2, fg='#40C3EC', bg='#635c57')
-entry.grid(row=0, column=0, columnspan=4, ipady=55)
+    # set fonts
+    f1 = font.Font(family='Calibri', size=12, weight='bold')
+    f2 = font.Font(family='Arial', size=16, weight='bold')
 
+    # entry unit and his settings
+    entry = Entry(window, width=26, relief='solid', borderwidth=2, fg='#40C3EC', bg='#635c57', justify=RIGHT, font=f2)
+    entry.insert(0, '0')
+    entry.grid(row=0, column=0, columnspan=4, ipady=55)
 
-def calc(x):
-    if x == "=":
-        str1 = "-+0123456789.*/)("
-        if entry.get()[0] not in str1:
-            entry.insert(END, "First symbol is not number")
-            messagebox.showerror("Error!", "You did not enter the number!")
-        try:
-            result = eval(entry.get())
-            entry.insert(END, "=" + str(result))
-        except:
-            entry.insert(END, "Error!")
-            messagebox.showerror("Error!", "Check the correctness of data")
-    elif x == "AC":
+    # Function 1
+    def add_digit(digit):
+        value = entry.get() + str(digit)
+        if value[0] == '0':
+            value = value[1:]
         entry.delete(0, END)
-    elif x == "+-":
-        if "=" in entry.get():
-            entry.delete(0, END)
-        try:
-            if entry.get()[0] == "-":
-                entry.delete(0)
-            else:
-                entry.insert(0, "-")
-        except IndexError:
-            pass
-    elif x == "sin":
-        entry.insert(END, "=" + str(math.sin(int(entry.get()))))
-    elif x == "cos":
-        entry.insert(END, "=" + str(math.cos(int(entry.get()))))
-    elif x == "pow":
-        entry.insert(END, "**")
-    elif x == "(":
-        entry.insert(END, "(")
-    elif x == ")":
-        entry.insert(END, ")")
-    elif x == "fact":
-        entry.insert(END, "=" + str(math.factorial(int(entry.get()))))
-    elif x == "sqrt":
-        entry.insert(END, "=" + str(math.sqrt(int(entry.get()))))
-    else:
-        if "=" in entry.get():
-            entry.delete(0, END)
-        entry.insert(END, x)
+        entry.insert(0, value)
 
+    # Function 2
+    def add_operation(operation):
+        value = entry.get()
+        if value[-1] in '+-*/':
+            value = value[:-1]
+        entry.delete(0, END)
+        entry.insert(0, value + operation)
 
-# names of buttons
-btn = ['sin', 'cos', 'tan', 'sqrt',
-       'fact', 'log', 'paw', 'exp',
-       '(', ')', 'AC', 'DEL',
-       '7', '8', '9', '-',
-       '4', '5', '6', '*',
-       '1', '2', '3', '/',
-       '0', '.', '=', '+']
+    # creating buttons
+    def creating_buttons_on_window():
+        # names of buttons
+        functions = ['sin', 'cos', 'tan', 'sqrt',
+                     'fact', 'log', 'paw', 'exp']
 
-# creating buttons
-row = 1
-col = 0
-for i in btn:
-    button = Button(window, text=i, command=lambda x=i: calc(x),
-                    width=3, bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-    button.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
-    col += 1
-    if col > 3:
+        addons = ['(', ')', 'AC', 'DEL']
+
+        numbers = ['7', '8', '9',
+                   '4', '5', '6',
+                   '1', '2', '3',
+                   '0']
+
+        result_and_dot = ['.', '=']
+
+        operations = ['-', '+', '*', '/']
+
+        # Now, we can place our buttons on window
+        row = 1
         col = 0
-        row += 1
+        for i in functions:
+            b_functions = Button(window, text=i, command=lambda x=i: add_digit(x), width=3,
+                                 bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+            b_functions.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
+
+        for j in addons:
+            b_addons = Button(window, text=j, command=lambda x=j: add_digit(x), width=3,
+                              bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+            b_addons.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
+
+        for k in numbers:
+            b_numbers = Button(window, text=k, command=lambda x=k: add_digit(x),
+                               width=3, bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+            b_numbers.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+            col += 1
+            if col > 2:
+                col = 0
+                row += 1
+
+        for r in result_and_dot:
+            b_numbers = Button(window, text=r, command=lambda x=r: add_digit(x), width=3,
+                               bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+            b_numbers.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+            col += 1
+
+        row = 4    # Переопределяем для нового отсчета (операции надо хуйнуть в столбик)
+        for t in operations:
+            b_operations = Button(window, text=t, command=lambda x=t: add_operation(x), width=3,
+                                  bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+            b_operations.grid(row=row, column=3, ipadx=11, ipady=10, padx=10, pady=12)
+            row += 1
+
+    creating_buttons_on_window()
+    window.grid_rowconfigure(0, weight=1)
+    window.mainloop()
+
 
 # mainloop
-window.grid_rowconfigure(0, weight=1)
-window.mainloop()
+if __name__ == "__main__":
+    main()

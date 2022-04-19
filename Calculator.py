c50e6999e25e3b053c6c1a51fc9a50267fc3f156
Abends2 <98419421+Abends2@users.noreply.github.com>
2022-04-19 22:47:17 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -29,6 +29,7 @@ def main():
     # Function 1
     def add_digit(digit):
         value = entry.get() + str(digit)
+        # Удаление нуля
         if value[0] == '0':
             value = value[1:]
         entry.delete(0, END)
@@ -42,20 +43,36 @@ def main():
         entry.delete(0, END)
         entry.insert(0, value + operation)
 
+    def add_functions():
+        return 0
+
+    # Function of main calculate (without eng_functions)
+    def calculate():
+        value = entry.get()
+        # Если вводим, например, 7 * и потом нажимаем равно, то будет 7 * 7, т.е 49
+        if value[-1] in '+-*/':
+            value = value + value[:-1]
+        entry.delete(0, END)
+        entry.insert(0, eval(value))
+
+    def cleaning_by_ac():
+        entry.delete(0, END)
+
+    def remove_last_elem_by_del():
+        entry.delete(len(entry.get()) - 1)
+
     # creating buttons
     def creating_buttons_on_window():
         # names of buttons
         functions = ['sin', 'cos', 'tan', 'sqrt',
                      'fact', 'log', 'paw', 'exp']
 
-        addons = ['(', ')', 'AC', 'DEL']
+        #staples = ['(', ')'] (LS, RS)
 
         numbers = ['7', '8', '9',
                    '4', '5', '6',
                    '1', '2', '3',
                    '0']
-
-        result_and_dot = ['.', '=']
 
         operations = ['-', '+', '*', '/']
 
@@ -71,29 +88,39 @@ def main():
                 col = 0
                 row += 1
 
-        for j in addons:
-            b_addons = Button(window, text=j, command=lambda x=j: add_digit(x), width=3,
-                              bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-            b_addons.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
-            col += 1
-            if col > 3:
-                col = 0
-                row += 1
+        RS = Button(window, text='(', width=3, command=lambda: add_digit('('),
+                     bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        RS.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+
+        LS = Button(window, text=')', command=lambda: add_digit(')'), width=3,
+                    bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        LS.grid(row=row, column=col + 1, ipadx=11, ipady=10, padx=10, pady=12)
+
+        DEL = Button(window, text='DEL', width=3, command=remove_last_elem_by_del,
+                         bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        DEL.grid(row=row, column=col+2, ipadx=11, ipady=10, padx=10, pady=12)
+
+        AC = Button(window, text='AC', command=cleaning_by_ac, width=3,
+                            bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        AC.grid(row=row, column=col+3, ipadx=11, ipady=10, padx=10, pady=12)
 
         for k in numbers:
             b_numbers = Button(window, text=k, command=lambda x=k: add_digit(x),
                                width=3, bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-            b_numbers.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+            b_numbers.grid(row=row+1, column=col, ipadx=11, ipady=10, padx=10, pady=12)
             col += 1
             if col > 2:
                 col = 0
                 row += 1
 
-        for r in result_and_dot:
-            b_numbers = Button(window, text=r, command=lambda x=r: add_digit(x), width=3,
+        # creating result and dot without array
+        dot_btn = Button(window, text='.', width=3, command=lambda: add_digit('.'),
                                bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-            b_numbers.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
-            col += 1
+        dot_btn.grid(row=row+1, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+
+        result_btn = Button(window, text='=', command=calculate, width=3,
+                               bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        result_btn.grid(row=row+1, column=col+1, ipadx=11, ipady=10, padx=10, pady=12)
 
         row = 4    # Переопределяем для нового отсчета (операции надо хуйнуть в столбик)
         for t in operations:

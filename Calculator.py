0271d589861a538ff7cca52a5436c6b58acd9ee4
Abends2 <98419421+Abends2@users.noreply.github.com>
2022-04-20 23:03:57 +0300

    Got It!

--- Calculator.py
+++ Calculator.py
@@ -3,39 +3,38 @@ import tkinter.font as font
 import math
 import sys
 
-# 40C3EC - color for text on Buttons
-# fffff0 - background color
-# 40C3EC - foreground color
+# 40C3EC - цвет текста на кнопках
+# fffff0 - цвет фона
+# 635c57 - фон окна ввода/вывода
 
 
 def main():
-    # window's settings
+    # Основная конфигурация окна
     window = Tk()
     window.title("Engineering Calculator")
-    window.iconbitmap('./assets/icon.ico')
+    window.iconbitmap(r'.\assets\icon.ico')
     window.geometry('345x750')
     window.resizable(width=False, height=False)
     window.configure(bg='#fffff0', borderwidth=8, relief='solid')
 
-    # set fonts
+    # Шрифты
     f1 = font.Font(family='Calibri', size=12, weight='bold')
     f2 = font.Font(family='Arial', size=16, weight='bold')
 
-    # entry unit and his settings
+    # Окно ввода/вывода и его конфигурация
     entry = Entry(window, width=26, relief='solid', borderwidth=2, fg='#40C3EC', bg='#635c57', justify=RIGHT, font=f2)
     entry.insert(0, '0')
     entry.grid(row=0, column=0, columnspan=4, ipady=55)
 
-    # Function 1
+    # Добавление цифр
     def add_digit(digit):
         value = entry.get() + str(digit)
-        # Удаление нуля
         if value[0] == '0':
             value = value[1:]
         entry.delete(0, END)
         entry.insert(0, value)
 
-    # Function 2
+    # Добавдение операционных символов
     def add_operation(operation):
         value = entry.get()
         if value[-1] in '+-*/':
@@ -43,6 +42,7 @@ def main():
         entry.delete(0, END)
         entry.insert(0, value + operation)
 
+    # Функция для вычисления математических функций
     def add_functions(x):
         value = entry.get()
         if x == 'sin':
@@ -64,24 +64,24 @@ def main():
         else:
             sys.exit()
 
-    # Function of main calculate (without eng_functions)
+    # Функция для базовых вычислений (+ - * /)
     def calculate():
         value = entry.get()
-        # Если вводим, например, 7 * и потом нажимаем равно, то будет 7 * 7, т.е 49
         if value[-1] in '+-*/':
             value = value + value[:-1]
         entry.delete(0, END)
         entry.insert(0, eval(value))
 
+    # Функция удаления (кнопка AC)
     def cleaning_by_ac():
         entry.delete(0, END)
 
+    # Функция удаления (кнопка DEL)
     def remove_last_elem_by_del():
         entry.delete(len(entry.get()) - 1)
 
-    # creating buttons
+    # Создание кнопок
     def creating_buttons_on_window():
-        # names of buttons
         functions = ['sin', 'cos', 'tan', 'sqrt',
                      'fact', 'ln', 'pow', 'exp']
 
@@ -92,7 +92,7 @@ def main():
 
         operations = ['-', '+', '*', '/']
 
-        # Now, we can place our buttons on window
+        # Размещаем кнопки
         row = 1
         col = 0
         for i in functions:
@@ -104,21 +104,21 @@ def main():
                 col = 0
                 row += 1
 
-        RS = Button(window, text='(', width=3, command=lambda: add_digit('('),
-                     bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-        RS.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
+        right_bracket = Button(window, text='(', width=3, command=lambda: add_digit('('),
+                               bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        right_bracket.grid(row=row, column=col, ipadx=11, ipady=10, padx=10, pady=12)
 
-        LS = Button(window, text=')', command=lambda: add_digit(')'), width=3,
-                    bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-        LS.grid(row=row, column=col + 1, ipadx=11, ipady=10, padx=10, pady=12)
+        left_bracket = Button(window, text=')', command=lambda: add_digit(')'), width=3,
+                              bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        left_bracket.grid(row=row, column=col + 1, ipadx=11, ipady=10, padx=10, pady=12)
 
-        DEL = Button(window, text='DEL', width=3, command=remove_last_elem_by_del,
+        btn_del = Button(window, text='DEL', width=3, command=remove_last_elem_by_del,
                          bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-        DEL.grid(row=row, column=col+2, ipadx=11, ipady=10, padx=10, pady=12)
+        btn_del.grid(row=row, column=col+2, ipadx=11, ipady=10, padx=10, pady=12)
 
-        AC = Button(window, text='AC', command=cleaning_by_ac, width=3,
-                            bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
-        AC.grid(row=row, column=col+3, ipadx=11, ipady=10, padx=10, pady=12)
+        btn_ac = Button(window, text='AC', command=cleaning_by_ac, width=3,
+                        bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+        btn_ac.grid(row=row, column=col+3, ipadx=11, ipady=10, padx=10, pady=12)
 
         for k in numbers:
             b_numbers = Button(window, text=k, command=lambda x=k: add_digit(x),
@@ -129,13 +129,12 @@ def main():
                 col = 0
                 row += 1
 
-        # creating result and dot without array
         dot_btn = Button(window, text='.', width=3, command=lambda: add_digit('.'),
-                               bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+                         bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
         dot_btn.grid(row=row+1, column=col, ipadx=11, ipady=10, padx=10, pady=12)
 
         result_btn = Button(window, text='=', command=calculate, width=3,
-                               bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
+                            bg='#635c57', fg='#40C3EC', borderwidth=5, font=f1)
         result_btn.grid(row=row+1, column=col+1, ipadx=11, ipady=10, padx=10, pady=12)
 
         row = 4
@@ -150,6 +149,5 @@ def main():
     window.mainloop()
 
 
-# mainloop
 if __name__ == "__main__":
     main()
